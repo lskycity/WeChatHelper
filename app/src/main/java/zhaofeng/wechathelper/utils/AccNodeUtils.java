935c6229eb22f915c571eb3aeb194bbef0e42911
lskycity <lskycity@foxmail.com>
2016-01-30 23:55:50 +0800

    add accnodeutils

---
+++ app/src/main/java/zhaofeng/wechathelper/utils/AccNodeUtils.java
@@ -0,0 +1,35 @@
+package zhaofeng.wechathelper.utils;
+
+import android.accessibilityservice.AccessibilityService;
+import android.text.TextUtils;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.Map;
+
+/**
+ * Created by liuzhaofeng on 2016/1/30.
+ */
+public class AccNodeUtils {
+    public void searchNodeByDescription(AccessibilityNodeInfo info, Map<String,AccessibilityNodeInfo> map, String... matchTexts) {
+        if (info != null) {
+            if (info.getChildCount() == 0) {
+                CharSequence desc = info.getContentDescription();
+                if(desc != null) {
+                    for(String text : matchTexts) {
+                        if(TextUtils.equals(desc, text)) {
+                            map.put(text, info);
+                        }
+                    }
+                }
+            } else {
+                int size = info.getChildCount();
+                for (int i = 0; i < size; i++) {
+                    AccessibilityNodeInfo childInfo = info.getChild(i);
+                    if (childInfo != null) {
+                        searchNodeByDescription(childInfo, map, matchTexts);
+                    }
+                }
+            }
+        }
+    }
+}
