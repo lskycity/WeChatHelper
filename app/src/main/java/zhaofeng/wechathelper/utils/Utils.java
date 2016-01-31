2854ff066d509dae9e87f7940ce763095c3c659e
lskycity <lskycity@foxmail.com>
2016-01-31 22:18:00 +0800

    add cardview

--- app/src/main/java/zhaofeng/wechathelper/utils/Utils.java
+++ app/src/main/java/zhaofeng/wechathelper/utils/Utils.java
@@ -1,13 +1,16 @@
 package zhaofeng.wechathelper.utils;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by kevinbest on 16/1/30.
  */
 public class Utils {
-    public static String getTimeString(long time)
-    {
-        return new Date(time).toLocaleString();
+    private static SimpleDateFormat FORMATTER = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+
+    public static String getTimeString(long time) {
+        return FORMATTER.format(new Date(time));
     }
 }
