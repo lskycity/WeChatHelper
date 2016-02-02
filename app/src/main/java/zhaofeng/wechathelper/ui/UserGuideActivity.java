3b255f4ca61cce7ae2bff12edb9229fae30d2e21
lskycity <lskycity@foxmail.com>
2016-02-02 20:22:45 +0800

    add user guide activity

---
+++ app/src/main/java/zhaofeng/wechathelper/ui/UserGuideActivity.java
@@ -0,0 +1,17 @@
+package zhaofeng.wechathelper.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import zhaofeng.wechathelper.R;
+
+/**
+ * Created by liuzhaofeng on 2016/2/2.
+ */
+public class UserGuideActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
