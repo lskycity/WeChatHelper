392e310d0c41640d8c18fa28d62a95af9ab9f297
lskycity <lskycity@foxmail.com>
2016-02-18 08:42:20 +0800

    refactor: change utils to dateUtils

--- app/src/main/java/zhaofeng/wechathelper/utils/DateUtils.java
+++ app/src/main/java/zhaofeng/wechathelper/utils/DateUtils.java
@@ -7,7 +7,7 @@ import java.util.Locale;
 /**
  * Created by kevinbest on 16/1/30.
  */
-public class Utils {
+public class DateUtils {
     private static SimpleDateFormat FORMATER = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
 
     public static String getTimeString(long time) {
