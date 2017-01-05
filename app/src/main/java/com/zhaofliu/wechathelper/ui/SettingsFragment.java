d04d93708e8058601c174b5bcf6bcf253f73c4a9
zhaofliu <lskycity@foxmail.com>
2017-01-05 17:42:11 +0800

    add new version reminder in settings

--- app/src/main/java/com/zhaofliu/wechathelper/ui/SettingsFragment.java
+++ app/src/main/java/com/zhaofliu/wechathelper/ui/SettingsFragment.java
@@ -1,14 +1,14 @@
 package com.zhaofliu.wechathelper.ui;
 
 
-import com.zhaofliu.wechathelper.R;
-import com.zhaofliu.wechathelper.utils.AppUtils;
-import com.zhaofliu.wechathelper.utils.SharedPreUtils;
-
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.v4.app.Fragment;
+
+import com.zhaofliu.wechathelper.R;
+import com.zhaofliu.wechathelper.utils.AppUtils;
+import com.zhaofliu.wechathelper.utils.SharedPreUtils;
 
 
 /**
@@ -29,6 +29,13 @@ public class SettingsFragment extends PreferenceFragment {
         getPreferenceManager().setSharedPreferencesName(SharedPreUtils.SHARED_PREFERENCE_NAME);
         Preference version = getPreferenceScreen().findPreference("app_version");
         version.setSummary(AppUtils.getVersionName(getActivity()));
+
+        int versionCode = SharedPreUtils.getInt(getActivity(), SharedPreUtils.KEY_LAST_DATE_CHECK_VERSION_CODE);
+        if(versionCode > AppUtils.getVersionCode(getActivity())) {
+            String versionName = SharedPreUtils.getString(getActivity(), SharedPreUtils.KEY_LAST_DATE_CHECK_VERSION_NAME);
+            version.setSummary(getString(R.string.current_version_and_have_new_version, AppUtils.getVersionName(getActivity()), versionName));
+
+        }
 
     }
 
