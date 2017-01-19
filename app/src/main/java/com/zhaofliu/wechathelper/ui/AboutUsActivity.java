5dc00ba0c6833fa461b08699e2d925d61e230c7c
zhaofliu <lskycity@foxmail.com>
2017-01-19 20:20:43 +0800

    modify user guide and about us.

--- app/src/main/java/com/zhaofliu/wechathelper/ui/AboutUsActivity.java
+++ app/src/main/java/com/zhaofliu/wechathelper/ui/AboutUsActivity.java
@@ -11,36 +11,29 @@ import com.zhaofliu.wechathelper.app.BaseActivity;
 
 import java.util.List;
 
-public class AboutUsActivity extends BaseActivity implements View.OnClickListener {
+public class AboutUsActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
-        findViewById(R.id.mail_link).setOnClickListener(this);
+//        findViewById(R.id.mail_link).setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.mail_link:
-                sendMail(getString(R.string.team_mail_address));
-                break;
-        }
-    }
+
 
     private void sendMail(String address) {
-        Intent emailIntent = new Intent(Intent.ACTION_SEND_MULTIPLE);
-        emailIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        emailIntent.setType("message/rfc822");
-        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{address});
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mail_subject));
-        emailIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.mail_content));
-
-        PackageManager pManager = getPackageManager();
-        List<ResolveInfo> info = pManager.queryIntentActivities(emailIntent, PackageManager.MATCH_DEFAULT_ONLY);
-        if (info != null) {
-            startActivity(Intent.createChooser(emailIntent, getString(R.string.mail_chooser_title)));
-        }
+//        Intent emailIntent = new Intent(Intent.ACTION_SEND_MULTIPLE);
+//        emailIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        emailIntent.setType("message/rfc822");
+//        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{address});
+//        emailIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mail_subject));
+//        emailIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.mail_content));
+//
+//        PackageManager pManager = getPackageManager();
+//        List<ResolveInfo> info = pManager.queryIntentActivities(emailIntent, PackageManager.MATCH_DEFAULT_ONLY);
+//        if (info != null) {
+//            startActivity(Intent.createChooser(emailIntent, getString(R.string.mail_chooser_title)));
+//        }
     }
 }
