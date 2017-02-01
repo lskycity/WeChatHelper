ea6e1da8f135ac093d7894a7271263475a808737
lskycity <lskycity@foxmail.com>
2017-02-01 21:08:41 +0800

    add code for restart the notification setting

--- app/src/main/java/com/zhaofliu/wechathelper/HunterNotificationService.java
+++ app/src/main/java/com/zhaofliu/wechathelper/HunterNotificationService.java
@@ -2,9 +2,7 @@ package com.zhaofliu.wechathelper;
 
 import android.annotation.TargetApi;
 import android.app.Notification;
-import android.app.Service;
 import android.os.Build;
-import android.provider.Settings;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.text.TextUtils;
@@ -17,6 +15,9 @@ import com.zhaofliu.wechathelper.apputils.Constants;
  */
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
 public class HunterNotificationService extends NotificationListenerService {
+
+    private static final String TAG = HunterNotificationService.class.getSimpleName();
+
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
@@ -32,4 +33,40 @@ public class HunterNotificationService extends NotificationListenerService {
     public void onNotificationRemoved(StatusBarNotification sbn) {
         super.onNotificationRemoved(sbn);
     }
+
+//    static void ensureCollectorRunning(Context context) {
+//        ComponentName collectorComponent = new ComponentName(context, /*NotificationListenerService Inheritance*/ HunterNotificationService.class);
+//        Log.v(TAG, "ensureCollectorRunning collectorComponent: " + collectorComponent);
+//        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+//        boolean serviceRunning = false;
+//        List<ActivityManager.RunningServiceInfo> runningServices = manager.getRunningServices(Integer.MAX_VALUE);
+//        if (runningServices == null ) {
+//            Log.w(TAG, "ensureCollectorRunning() runningServices is NULL");
+//            return;
+//        }
+//        for (ActivityManager.RunningServiceInfo service : runningServices) {
+//            if (service.service.equals(collectorComponent)) {
+//                Log.w(TAG, "ensureCollectorRunning service - pid: " + service.pid + ", currentPID: " + Process.myPid() + ", clientPackage: " + service.clientPackage + ", clientCount: " + service.clientCount
+//                        + ", clientLabel: " + ((service.clientLabel == 0) ? "0" : "(" + context.getString(service.clientLabel) + ")"));
+//                if (service.pid == Process.myPid() /*&& service.clientCount > 0 && !TextUtils.isEmpty(service.clientPackage)*/) {
+//                    serviceRunning = true;
+//                }
+//            }
+//        }
+//        if (serviceRunning) {
+//            Log.d(TAG, "ensureCollectorRunning: collector is running");
+//            return;
+//        }
+//        Log.d(TAG, "ensureCollectorRunning: collector not running, reviving...");
+//        toggleNotificationListenerService(context);
+//    }
+//
+//    private static void toggleNotificationListenerService(Context context) {
+//        Log.d(TAG, "toggleNotificationListenerService() called");
+//        ComponentName thisComponent = new ComponentName(context, /*getClass()*/ HunterNotificationService.class);
+//        PackageManager pm = context.getPackageManager();
+//        pm.setComponentEnabledSetting(thisComponent, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+//        pm.setComponentEnabledSetting(thisComponent, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+//
+//    }
 }
