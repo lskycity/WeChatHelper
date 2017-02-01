9f22fd89071992306ecaaf59c78dc2986ae16ec0
lskycity <lskycity@foxmail.com>
2017-02-01 19:01:09 +0800

    add share to friend shortcuts

--- app/src/main/java/com/zhaofliu/wechathelper/ui/ShareActivity.java
+++ app/src/main/java/com/zhaofliu/wechathelper/ui/ShareActivity.java
@@ -1,10 +1,7 @@
 package com.zhaofliu.wechathelper.ui;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,34 +10,17 @@ import com.zhaofliu.wechathelper.MainActivity;
 import com.zhaofliu.wechathelper.R;
 import com.zhaofliu.wechathelper.app.BaseActivity;
 import com.zhaofliu.wechathelper.apputils.Constants;
-import com.zhaofliu.wechathelper.apputils.ServiceUtils;
-import com.zhaofliu.wechathelper.utils.AppUtils;
 import com.zhaofliu.wechathelper.utils.IntentUtils;
 
 
 /**
- * Created by zhaofliu on 1/6/17.
+ * Created by zhaofliu on 2/1/17.
  *
  *
  */
 
-public class OpenAccessibilityActivity extends BaseActivity implements View.OnClickListener {
+public class ShareActivity extends BaseActivity implements View.OnClickListener {
 
-    private static final String KEY_UNLOCK = "key_unlock";
-
-    public static void startForUnlockScreen(Context context) {
-        Intent intent = new Intent(context, OpenAccessibilityActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.putExtra(KEY_UNLOCK, true);
-        context.startActivity(intent);
-    }
-
-    private Handler finishHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            finish();
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,34 +29,18 @@ public class OpenAccessibilityActivity extends BaseActivity implements View.OnCl
         setContentView(R.layout.activity_open_acc);
 
         TextView tv = (TextView) findViewById(R.id.display_text);
+        tv.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.download_url,0,0);
+        tv.setText(R.string.let_your_friend_download);
+
         Button userGuide = (Button) findViewById(R.id.user_guide);
         Button manPage = (Button) findViewById(R.id.main_page);
         Button shared = (Button) findViewById(R.id.share_to_friend);
 
+        userGuide.setOnClickListener(this);
+        manPage.setOnClickListener(this);
+        shared.setOnClickListener(this);
 
-        if(!ServiceUtils.isAccessibilityEnabled(this)) {
-            ServiceUtils.openAccServiceSetting(this);
-            finish();
-        } else {
-            userGuide.setOnClickListener(this);
-            manPage.setOnClickListener(this);
-            shared.setOnClickListener(this);
-        }
 
-        boolean unlockFlag = getIntent().getBooleanExtra(KEY_UNLOCK, false);
-        if(unlockFlag) {
-            tv.setText(R.string.unlocking_screen);
-            userGuide.setVisibility(View.GONE);
-            manPage.setVisibility(View.GONE);
-
-            if(getSupportActionBar()!=null) {
-                getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-
-            }
-
-            AppUtils.unlockScreen(this, true);
-            finishHandler.sendEmptyMessageDelayed(1, 1000);
-        }
     }
 
     @Override
@@ -93,6 +57,7 @@ public class OpenAccessibilityActivity extends BaseActivity implements View.OnCl
             IntentUtils.shareText(this, getString(R.string.share_to_friend), Constants.WECHAT_VERSION_URL);
         }
     }
+
 
     @Override
     public boolean onSupportNavigateUp() {
